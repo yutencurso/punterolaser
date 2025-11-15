9fc1b2ca4c9d9674e827939e4eca84828506471e
Daniel <yutencurso@gmail.com>
2025-11-15 11:56:59 +0100

    Delete dist/index.js

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-const body = document.body;
-        body.addEventListener('mousemove', (evento) => {
-            const x = evento.clientX; 
-            const y = evento.clientY; 
-            const efecto = document.getElementById('efecto');
-            efecto.style.position = "fixed";
-            efecto.style.transform = `translate(${x}px, ${y}px)`;
-        });

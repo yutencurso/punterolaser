01b828b2eb65f927db7130fb072f47bd3aea6b27
Daniel <yutencurso@gmail.com>
2025-11-15 12:27:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ efecto.style.position = "fixed";
 efecto.style.pointerEvents = "none";
 efecto.style.zIndex = "99999";
 const style = document.createElement('style');
-style.innerHTML = `* { cursor: none !important; }`;
+style.innerHTML = `html, body, *, a, button { cursor: none !important; }`;
 document.head.appendChild(style);
-    body.addEventListener('mousemove', (evento) => {
-        const x = evento.clientX; 
-        const y = evento.clientY; 
-        efecto.style.transform = `translate(${x}px, ${y}px)`;
-    });
+document.documentElement.style.cursor = 'none';
+window.addEventListener('mousemove', (evento) => {
+    const x = evento.clientX;
+    const y = evento.clientY;
+    efecto.style.transform = `translate(${x}px, ${y}px)`;
+});

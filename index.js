0c6cd0ea6313580380c5844e060e9fcbc6db6b7c
Daniel <yutencurso@gmail.com>
2025-11-15 12:12:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const efecto = document.getElementById('efecto');
 efecto.style.position = "fixed";
 efecto.style.pointerEvents = "none";
 body.style.cursor = "none";
+efecto.style.userSelect = "none";
     body.addEventListener('mousemove', (evento) => {
         const x = evento.clientX; 
         const y = evento.clientY; 

50a1009b20e6a3841a701045445cd18a4852fc58
Daniel <yutencurso@gmail.com>
2025-11-15 11:57:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const body = document.body;
+const efecto = document.getElementById('efecto');
+efecto.style.position = "fixed";
+efecto.style.pointerEvents = "none";
+body.style.cursor = "none";
+    body.addEventListener('mousemove', (evento) => {
+        const x = evento.clientX; 
+        const y = evento.clientY; 
+        efecto.style.transform = `translate(${x}px, ${y}px)`;
+    });

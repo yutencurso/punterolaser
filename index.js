c212a13a6e2aa8ce503dce99506268ff2bd2d5e6
Daniel <yutencurso@gmail.com>
2025-11-15 09:09:54 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const body = document.body;
+        body.addEventListener('mousemove', (evento) => {
+            const x = evento.clientX; 
+            const y = evento.clientY; 
+            const efecto = document.getElementById('efecto');
+            efecto.style.transform = `translate(${x}px, ${y}px)`;
+        });
